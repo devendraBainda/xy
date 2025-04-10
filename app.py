696790a943a917cfb256557ae50181ce815087e4
devendraBainda <devendrabainda192@gmail.com>
2025-04-10 10:03:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,5 +103,6 @@ def upload_file():
 def results():
     return render_template('results.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
